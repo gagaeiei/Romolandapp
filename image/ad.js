ee352cec41f21abcbafbde941d8b5bbb1d0f4ca1
gagaeiei <160008396+gagaeiei@users.noreply.github.com>
2024-03-14 11:21:32 +0700

    ad

--- image/ad.js
+++ image/ad.js
@@ -1,3 +1,5 @@
+[
 { "id": "1",  "uri": "https://github.com/gagaeiei/Romolandapp/blob/af512e139f48002822db2a44881c438cec1cd505/assets/Ad/Ad1.jpg" },
 { "id": "2", "uri": "https://github.com/gagaeiei/Romolandapp/blob/af512e139f48002822db2a44881c438cec1cd505/assets/Ad/Ad2.jpg" },
 { "id": "3",  "uri": "https://github.com/gagaeiei/Romolandapp/blob/af512e139f48002822db2a44881c438cec1cd505/assets/Ad/Ad3.png" },
+]
