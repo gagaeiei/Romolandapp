6c701596e78011d08c97375d1166dce5b8868c39
gagaeiei <160008396+gagaeiei@users.noreply.github.com>
2024-03-14 11:17:43 +0700

    ad

--- components/Ad.js
+++ components/Ad.js
@@ -1,13 +1,29 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { FlatList, Image, Text, View } from "react-native";
 
 export default function Ad(props) {
-    const adData = [
+    const tours = [
         { "id": "1",  "uri": "https://th.bing.com/th/id/R.c875083cf80d963717a86c399392be58?rik=FBFL3Y2ylEM4BQ&pid=ImgRaw&r=0" },
         { "id": "2", "uri": "https://beeclean.app/assets/images/webog-1.jpg" },
         { "id": "3",  "uri": "https://ddq1mzvkg4fn7.cloudfront.net/2023/03/Untitled-1-01.jpg" },
-        { "id": "4",  "uri": "https://th.bing.com/th/id/R.20b703f8d062f2a455206d0751428b2b?rik=lx9OOq7jgYAJ6w&pid=ImgRaw&r=0" },
+        { "id": "4",  "uri": "https://th.bing.com/th/id/R.20b703f8d062f2a455206d0751428b2b?rik=lx9OOq7jgYAJ6w&pid=ImgRaw&r=0" }
     ];
+    const [onlineTours, setOnlineTours] = useState([]);
+    const loadOnlineTours = async () => {
+        try {
+            let promise = await fetch('https://raw.githubusercontent.com/gagaeiei/Romolandapp/master/image/ad.js');
+            let data = await promise.json();
+            console.log("Load Data : ", data);
+            //SET STATE
+            setOnlineTours(data);
+        } catch (error) {
+            console.log("ERROR : ", error);
+        }
+    }
+    useEffect(() => {
+        loadOnlineTours();
+    }, []);
+            
 
     return (
         <View style={props.style}>
@@ -15,7 +31,7 @@ export default function Ad(props) {
 
                 <FlatList
                     horizontal={true}
-                    data={adData}
+                    data={onlineTours}
                     renderItem={({ item }) => (
                         <View style={{ marginRight: -35,padding:30 }}>
                             <Image style={{ width: 250, height: 150, borderRadius: 10 }} source={{ uri: item.uri }} />
--- image/ad.js
+++ image/ad.js
@@ -1,4 +1,3 @@
-{ "id": "1",  "uri": "https://th.bing.com/th/id/R.c875083cf80d963717a86c399392be58?rik=FBFL3Y2ylEM4BQ&pid=ImgRaw&r=0" },
-{ "id": "2", "uri": "https://beeclean.app/assets/images/webog-1.jpg" },
-{ "id": "3",  "uri": "https://ddq1mzvkg4fn7.cloudfront.net/2023/03/Untitled-1-01.jpg" },
-{ "id": "4",  "uri": "https://th.bing.com/th/id/R.20b703f8d062f2a455206d0751428b2b?rik=lx9OOq7jgYAJ6w&pid=ImgRaw&r=0" },
+{ "id": "1",  "uri": "https://github.com/gagaeiei/Romolandapp/blob/af512e139f48002822db2a44881c438cec1cd505/assets/Ad/Ad1.jpg" },
+{ "id": "2", "uri": "https://github.com/gagaeiei/Romolandapp/blob/af512e139f48002822db2a44881c438cec1cd505/assets/Ad/Ad2.jpg" },
+{ "id": "3",  "uri": "https://github.com/gagaeiei/Romolandapp/blob/af512e139f48002822db2a44881c438cec1cd505/assets/Ad/Ad3.png" },
