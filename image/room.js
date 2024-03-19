3e96d03136ca6e0957d20889af6fc75b12c859aa
gagaeiei <160008396+gagaeiei@users.noreply.github.com>
2024-03-19 16:28:34 +0700

    Update room.js

--- image/room.js
+++ image/room.js
@@ -3,6 +3,6 @@
     { "id": "2", "uri": "https://raw.githubusercontent.com/gagaeiei/Romolandapp/master/assets/room/fit.jpg" },
     { "id": "3", "uri": "https://raw.githubusercontent.com/gagaeiei/Romolandapp/master/assets/room/music.jpg" },
     { "id": "4", "uri": "https://raw.githubusercontent.com/gagaeiei/Romolandapp/master/assets/room/pool-table.jpg" },
-    { "id": "4", "uri": "https://raw.githubusercontent.com/gagaeiei/Romolandapp/master/assets/room/work1.jpg" },
-    { "id": "5", "uri": "https://raw.githubusercontent.com/gagaeiei/Romolandapp/master/assets/room/work2.jpg" },
+    { "id": "5", "uri": "https://raw.githubusercontent.com/gagaeiei/Romolandapp/master/assets/room/work1.jpg" },
+    { "id": "6", "uri": "https://raw.githubusercontent.com/gagaeiei/Romolandapp/master/assets/room/work2.jpg" },
 ]
