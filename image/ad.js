48d84d8da3255bcb21ddd2a7ddc36b471d957e71
gagaeiei <160008396+gagaeiei@users.noreply.github.com>
2024-03-20 13:46:26 +0700

    Delete image directory

--- image/ad.js
+++
@@ -1,5 +0,0 @@
-[
-{ "id": "1",  "uri": "https://raw.githubusercontent.com/gagaeiei/Romolandapp/master/assets/Ad/Ad1.jpg" },
-{ "id": "2", "uri": "https://raw.githubusercontent.com/gagaeiei/Romolandapp/master/assets/Ad/Ad2.jpg" },
-{ "id": "3",  "uri": "https://raw.githubusercontent.com/gagaeiei/Romolandapp/master/assets/Ad/Ad3.png" },
-]
